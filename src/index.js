b78df83ee8cfb7d5226dfb6f1d150ee06a579a5d
timmyCturner <kuenytim@gmail.com>
2022-01-20 13:48:53 -0800

    deploy_v5

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-
-app.use(express.static(__dirname + '/public'))
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
